ab5bd1d27e8d687ec5be62835bead0982d6c4eb6
johnny-walker <albert_hsu@cyberlink.com>
2021-12-16 17:37:19 +0800

    update launch.json

--- tool/video_completion.py
+++ tool/video_completion.py
@@ -799,24 +799,24 @@ if __name__ == '__main__':
     parser.add_argument('--seamless', action='store_true', help='Whether operate in the gradient domain')
     parser.add_argument('--edge_guide', action='store_true', help='Whether use edge as guidance to complete flow')
     parser.add_argument('--mode', default='object_removal', help="modes: object_removal / video_extrapolation")
-    parser.add_argument('--path', default='data/tennis', help="dataset for evaluation")
-    parser.add_argument('--path_mask', default='data/tennis_mask', help="mask for object removal")
-    parser.add_argument('--outroot', default='result/', help="output directory")
+    parser.add_argument('--path', default='../data/tennis', help="dataset for evaluation")
+    parser.add_argument('--path_mask', default='../data/tennis_mask', help="mask for object removal")
+    parser.add_argument('--outroot', default='../result/', help="output directory")
     parser.add_argument('--consistencyThres', dest='consistencyThres', default=np.inf, type=float, help='flow consistency error threshold')
     parser.add_argument('--alpha', dest='alpha', default=0.1, type=float)
     parser.add_argument('--Nonlocal', action='store_true', help='Whether use edge as guidance to complete flow')
 
     # RAFT
-    parser.add_argument('--model', default='weight/raft-things.pth', help="restore checkpoint")
+    parser.add_argument('--model', default='../weight/raft-things.pth', help="restore checkpoint")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
 
     # Deepfill
-    parser.add_argument('--deepfill_model', default='weight/imagenet_deepfill.pth', help="restore checkpoint")
+    parser.add_argument('--deepfill_model', default='../weight/imagenet_deepfill.pth', help="restore checkpoint")
 
     # Edge completion
-    parser.add_argument('--edge_completion_model', default='weight/edge_completion.pth', help="restore checkpoint")
+    parser.add_argument('--edge_completion_model', default='../weight/edge_completion.pth', help="restore checkpoint")
 
     # extrapolation
     parser.add_argument('--H_scale', dest='H_scale', default=2, type=float, help='H extrapolation scale')
