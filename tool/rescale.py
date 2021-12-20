6fea6b8eb9d748849c6dbf5ea4e2fa96b7266d3a
johnny-walker <albert_hsu@cyberlink.com>
2021-12-20 17:31:42 +0800

    update data

--- tool/rescale.py
+++ tool/rescale.py
@@ -3,6 +3,57 @@ import os
 import argparse
 import glob
 
+class VideoWriter(object):
+    """
+    Video writer which handles video recording overhead
+    Usage:
+        object creation: provide path to write
+        write:
+        release:
+    """
+    def __init__(self, video_file, fps=25, scale=1.0):
+        """
+
+        :param video_file: path to write video. Perform nothing in case of None
+        :param fps: frame per second
+        :param scale: resize scale
+        """
+        self.video_file = video_file
+        self.fps = fps
+        self.writer = None
+        self.scale = scale
+
+    def write(self, frame):
+        """
+
+        :param frame: numpy array, (H, W, 3), BGR, frame to write
+        :return:
+        """
+        h, w = frame.shape[:2]
+        h_rsz, w_rsz = int(h * self.scale), int(w * self.scale)
+        frame = cv2.resize(frame, (w_rsz, h_rsz))
+        if self.writer is None:
+            video_dir = os.path.dirname(os.path.realpath(self.video_file))
+            if not os.path.exists(video_dir):
+                os.makedirs(video_dir)
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            self.writer = cv2.VideoWriter(self.video_file, fourcc, self.fps,
+                                          tuple(frame.shape[1::-1]))
+        self.writer.write(frame)
+
+    def release(self):
+        """
+        Manually release
+        :return:
+        """
+        if self.writer is None:
+            return
+        self.writer.release()
+
+    def __del__(self):
+        self.release()
+
+
 def main(args):
     # Loads frames.
     filename_list = glob.glob(os.path.join(args.path, '*.png')) + \
